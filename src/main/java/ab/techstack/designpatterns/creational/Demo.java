fa73eb38be30c63be6c6755a842ed83cc5cb4b03
Amol Bhople <amolbhople@gmail.com>
2021-03-28 00:37:38 +0530

    first commit

---
+++ src/main/java/ab/techstack/designpatterns/creational/Demo.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021.
+ */
+
+package ab.techstack.designpatterns.creational;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Testiting nng");
+    }
+}
---
+++ src/test/java/ab/techstack/designpatterns/creational/DemoTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021.
+ */
+
+package ab.techstack.designpatterns.creational;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoTest {
+
+    @Test
+    public void testOne(){
+        assertTrue(true);
+    }
+
+}
